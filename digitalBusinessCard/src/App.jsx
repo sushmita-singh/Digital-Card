e99d72d9b0bacf35e89b49eb343d67d4b497c692
sushmita-singh <sushmitas326@gmail.com>
2024-02-26 21:06:57 +0530

    Basic layout complete with CSS

--- digitalBusinessCard/src/App.jsx
+++ digitalBusinessCard/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div className='main-component'>
+    <div className='header'>
+      <img className='profile-pic' src='../images/profile-photo.jpg' alt="profile-photo" />
+    </div>
+    <div className='profile'>
+      <h1 className='name'>Sushmita</h1>
+      <h3 className='designation'>Frontend Developer</h3>
+      <h4 className='email'>sushmitas586@gmail.com</h4>
+    </div>
+    <div className='cta'>
+      <button className='btn-light'><img className='logo-btn' src='../images/envelope-icon.png' />Email</button>
+      <button className='btn-dark'><img className='logo-btn linkedIn' src='../images/linkedin-square-icon.png' />LinkedIn</button>
+    </div>
+    <div className='data'>
+      <h2 className='subheading'>About</h2>
+      <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, debitis!</p>
+      <h2 className='subheading'>Interests</h2>
+      <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, debitis!</p>
+    </div>
+    <div className='footer'>
+      <img className='logo' src='../images/x-social-media-logo-icon.png'  alt='Twitter' />
+      <img className='logo' src='../images/medium-round-icon.png' alt='Medium' />
+      <img className='logo' src='./images/github-icon.png' alt='Github' />
+    </div>
+  </div>
   )
 }
 
